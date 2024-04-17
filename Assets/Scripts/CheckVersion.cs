eb2ce24d0d91a477a1b4718d2b992e6e390b2b41
Xider <60540024+aMask-studio@users.noreply.github.com>
2024-04-17 19:55:42 +0700

    v4

--- Assets/Scripts/CheckVersion.cs
+++ Assets/Scripts/CheckVersion.cs
@@ -66,21 +66,51 @@ public class CheckVersion : MonoBehaviour
         else
         {
             txtLoading.text = "Скачано. APK файл находится в папке Download";
+            installApp(Application.persistentDataPath + "/LiteraryTriangle.apk");
             File.Copy(Application.persistentDataPath + "/LiteraryTriangle.apk", "/storage/emulated/0/Download" + "/LiteraryTriangle.apk", true);
             //File.Move(Application.persistentDataPath + "/LiteraryTriangle.apk","/storage/emulated/0/Download" + "/LiteraryTriangle.apk");
             UnityEngine.Debug.Log("success");
-            installApp(Application.persistentDataPath + "/LiteraryTriangle.apk");
         }
 
         yield return wwwRequest;
     }
-    private bool installApp(string apkPath)
+    public void installApp(string apkPath)
     {
-        bool success = true;
+        //bool success = true;
         //GameObject.Find("TextDebug").GetComponent<Text>().text = "Installing App";
         txtLoading.text = "APK устанавливается";
 
-        try
+        //Get Activity then Context
+        AndroidJavaClass unityPlayer = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
+        AndroidJavaObject currentActivity = unityPlayer.GetStatic<AndroidJavaObject>("currentActivity");
+        AndroidJavaObject unityContext = currentActivity.Call<AndroidJavaObject>("getApplicationContext");
+
+        //Get the package Name
+        string packageName = unityContext.Call<string>("getPackageName");
+        string authority = packageName + ".fileprovider";
+
+        AndroidJavaClass intentObj = new AndroidJavaClass("android.content.Intent");
+        string ACTION_VIEW = intentObj.GetStatic<string>("ACTION_VIEW");
+        AndroidJavaObject intent = new AndroidJavaObject("android.content.Intent", ACTION_VIEW);
+
+
+        int FLAG_ACTIVITY_NEW_TASK = intentObj.GetStatic<int>("FLAG_ACTIVITY_NEW_TASK");
+        int FLAG_GRANT_READ_URI_PERMISSION = intentObj.GetStatic<int>("FLAG_GRANT_READ_URI_PERMISSION");
+
+        //File fileObj = new File(String pathname);
+        AndroidJavaObject fileObj = new AndroidJavaObject("java.io.File", apkPath);
+        //FileProvider object that will be used to call it static function
+        AndroidJavaClass fileProvider = new AndroidJavaClass("androidx.core.content.FileProvider");
+        //getUriForFile(Context context, String authority, File file)
+        AndroidJavaObject uri = fileProvider.CallStatic<AndroidJavaObject>("getUriForFile", unityContext, authority, fileObj);
+
+        intent.Call<AndroidJavaObject>("setDataAndType", uri, "application/vnd.android.package-archive");
+        intent.Call<AndroidJavaObject>("addFlags", FLAG_ACTIVITY_NEW_TASK);
+        intent.Call<AndroidJavaObject>("addFlags", FLAG_GRANT_READ_URI_PERMISSION);
+        currentActivity.Call("startActivity", intent);
+        txtLoading.text = "APK установлен";
+
+        /*try
         {
             //Get Activity then Context
             AndroidJavaClass unityPlayer = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
@@ -119,8 +149,8 @@ public class CheckVersion : MonoBehaviour
             txtLoading.text = "Ошибка" + e.Message;
             //GameObject.Find("TextDebug").GetComponent<Text>().text = "Error: " + e.Message;
             success = false;
-        }
+        }*/
 
-        return success;
+        //return success;
     }
 }
